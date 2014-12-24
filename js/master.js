e2be6646993bc2a8b7bb643f6816d22d03987aa7
Zeyi Zhao <zeyi.zhao@gmail.com>
2014-12-24 16:45:01 +0800

    store basket info into cookie

--- js/master.js
+++ js/master.js
@@ -1,3 +1,22 @@
+function updatebasket(){
+  var basket = [];
+  if ($.cookie("basket") != undefined)
+    basket = JSON.parse($.cookie("basket"));
+  $('.cartnum').text('Cart(' + basket.length + ')')
+  if(basket.length == 0) {
+    $('.basketinfo>li:eq(0)>h3').text('没有商品')
+  }
+  else {
+    $('.basketinfo').empty();
+    $('.basketinfo>li:eq(0)>h3').text('共有' + basket.length + '种商品')
+    for (var i = 0; i < basket.length; i++) {
+      $('.basketinfo').append(
+        $('<li>').append($('<p>').text(basket[i].name + '---' + basket[i].num)))
+    }
+  }
+}
+
+
 $('.account').click(function(){
   if($.cookie('user_uri') == undefined) {
     window.location.href = "login.html";
@@ -11,4 +30,5 @@ $(function(){
     $('.loginbtn').hide();
     $('.signupbtn').hide();
   }
+  updatebasket();
 })
